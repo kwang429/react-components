6a781f1be50e00d2be7b0cde97eb82c19a4f3a44
Katherine Wang <katherine.wang429@gmail.com>
2019-11-04 20:32:28 -0800

    Finish React mini-sprint

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // TODO
+//jshint esversion:6
+class GroceryItemList extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            done: false
+        };
+    }
+
+    onListItemClick() {
+        this.setState({
+            done: !this.state.done
+        });
+    }
+
+    render() {
+        var style = {
+            textDecoration: this.state.done ? 'line-through' : 'none'
+        }
+        return (
+            <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.groceries}</li>
+        )
+    }
+}
+    
+var GroceryList = (props) => (
+    <ul>
+        {props.groceries.map(grocery =>
+            <GroceryItemList groceries={grocery} />
+        )}
+    </ul>
+);
+
+var App = () => (
+    <div>
+        <h2>My Grocery List</h2>
+        <GroceryList groceries={['kale', 'cucumber', 'choco pie']}/>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
+    
